d2c07cc3770ffc2947ba11ec343423f51292ae57
starlegendary <31045265+starlegendary@users.noreply.github.com>
2020-05-02 00:50:56 +0800

    Update display.cpp

--- display.cpp
+++ display.cpp
@@ -5,54 +5,54 @@
 #include <ctime>
 #include <cstdlib>
 #include "datas.h"
+
 using namespace std;
 
-void nth(){
-    cout<< ' '<<blanck<<' '<<blanck;
+void nth(bool sth){if(sth) cout<<" "<< space <<" " <<space<<" ";};
+void end(bool sth){if(sth) cout<<endl;}
+void aaa(bool sth, vector<vector<rooms>> board,int i, int j){
+    if(sth) cout<<corn<<wall<<board[i][j].door.f<<wall<<corn; else nth(true);};
+void abc(bool sth, vector<vector<rooms>> board,int i, int j){
+    if(sth) cout<<board[i][j].door.l<<space<<board[i][j].type<<space<<board[i][j].door.r; else nth(true);};
+void ccc(bool sth, vector<vector<rooms>> board,int i, int j){
+    if(sth) cout<<corn<<wall<<board[i][j].door.b<<wall<<corn; else nth(true);};
+void b(bool sth){if(sth) cout<<brick; else nth(true);};
+bool inside(int i, int j){
+    return ((j < S)&&(j >= 0)&&(i >= 0)&&(i < S));
 };
-void aline(rooms room, bool empty, char t){
-    if(empty){nth();};
-    if(t == 'z')
-    {cout<<edge<<wall<<room.door.f<<wall<<edge;}
-    if(t == 'a')
-    {cout<<edge<<wall<<room.door.f<<wall;}
-    else if(t == 'y')
-    {cout<< '|'<<blanck<<' '<<blanck<<'|';}
-    else if(t == 'b')
-    {cout<< '|'<<blanck<<' '<<blanck;}
-    else if(t == 'x'){cout<<room.door.l<<blanck<<room.type<<blanck<<room.door.r;}
-    else if(t == 'c')
-    {cout<<room.door.l<<blanck<<room.type<<blanck;}
-    else if (t == 'w')
-    {cout<<edge<<wall<<room.door.b<<wall<<edge;}
-    else if (t == 'd')
-    {cout<<edge<<wall<<room.door.b<<wall;};
-    if(empty){cout<<endl;};};
-void three(vector<vector<rooms>> board,int i, int j, bool empty,char a,char b,char c){
-    aline(board[i][j-1], empty,a);
-    aline(board[i][j  ], empty,b);
-    aline(board[i][j+1], empty,c);};
 
-void adisplay(vector<vector<rooms>> board,int i, int j){
-    aline(board[i][j], true,'y');
-    aline(board[i][j], true,'y');
-    aline(board[i][j], true,'x');
-    aline(board[i][j], true,'y');
-    aline(board[i][j], true,'y');
+void bbb(bool sth,vector<vector<rooms>> board,int i, int j){
+    b((sth && inside(i-1,j)));
+    b((inside(i  ,j)));
+    b((sth && inside(i+1,j)));end(true);
+    b((sth && inside(i-1,j)));
+    b((inside(i  ,j)));
+    b((sth && inside(i+1,j)));end(true);
 };
-void bdisplay(vector<vector<rooms>> board,int i, int j){
-    three(board,i,j, false,'a','a','z');cout<<endl;
-    for(int j = 0; j<2; j++){
-        three(board,i,j, false,'b','b','y');cout<<endl;};
-    three(board,i,j, false,'c','c','x');cout<<endl;
-    for(int j = 0; j<2; j++){
-        three(board,i,j, false,'b','b','y');cout<<endl;};
-    three(board,i,j, false,'d','d','z');cout<<endl;
+
+void top(bool sth,vector<vector<rooms>> board,int i, int j){
+    aaa((sth && inside(i-1,j)),board,i-1,j);
+    aaa((inside(i  ,j)),board,i  ,j);
+    aaa((sth && inside(i+1,j)),board,i+1,j);end(true);
+};
+void bot(bool sth,vector<vector<rooms>> board,int i, int j){
+    ccc((sth && inside(i-1,j)),board,i-1,j);
+    ccc((inside(i  ,j)),board,i  ,j);
+    ccc((sth && inside(i+1,j)),board,i+1,j);end(true);
+};
+void mid(bool sth,vector<vector<rooms>> board,int i, int j){
+    abc((sth && inside(i-1,j)),board,i-1,j);
+    abc((inside(i  ,j)),board,i  ,j);
+    abc((sth && inside(i+1,j)),board,i+1,j);end(true);
 };
 void display(vector<vector<rooms>> board,int i, int j){
-    aline(board[i-1][j], true,'z');
-    adisplay(board,i-1,j);
-    bdisplay(board,i,j);
-    adisplay(board,i+1,j);
-    aline(board[i+1][j], true,'w');
+    top(false,board,i,j-1);bbb(false,board,i,j-1);
+    mid(false,board,i,j-1);bbb(false,board,i,j-1);
+    bot(false,board,i,j-1);
+    top(true,board,i,j);bbb(true,board,i,j);
+    mid(true,board,i,j);bbb(true,board,i,j);
+    bot(true,board,i,j);
+    top(false,board,i,j+1);bbb(false,board,i,j+1);
+    mid(false,board,i,j+1);bbb(false,board,i,j+1);
+    bot(false,board,i,j+1);
 };
