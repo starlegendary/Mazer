d4a4ff4a59f16c21df09125460ed523827776764
stacie39 <61752752+stacie39@users.noreply.github.com>
2020-05-05 03:26:07 +0800

    Delete random_item.cpp

--- random_item.cpp
+++
@@ -1,9 +0,0 @@
-#include "datas.h"
-
-string random_item(){
-  srand(time(NULL));
-  auto it = items.begin();
-  advance(it, rand() % items.size());
-  string rditem_name = it->first;
-  return rditem_name;
-}
