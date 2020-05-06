ac65fb6393f24760b142e0b0c51a22a96499d4c4
stacie39 <61752752+stacie39@users.noreply.github.com>
2020-05-06 23:31:01 +0800

    Update datas.h

--- datas.h
+++ datas.h
@@ -7,8 +7,9 @@
 #include <string>
 #include <ctime>
 #include <cstdlib>
-
 using namespace std;
+
+//int S = 5;
 
 struct position
 {
@@ -18,6 +19,7 @@ struct status
 {
     position pos;
     int h, d, v;
+
 };
 struct doors
 {
@@ -27,17 +29,77 @@ struct doors
 struct rooms
 {
     doors door;
-    char  type = 'N';
+    string type = "N" ;
+    string item_name ="NULL";
     int   pass = 0;
 };
 
-void add_door(rooms&);
+void add_door(rooms&,int);
+string random_item(int);
 void move(status& , status&);
 void define_items();
-void define_player_monster();
-void display(vector<vector<rooms>>,int, int);
-string random_item(int);
+void define_player_monster(int);
 status MakeStatus(int, int, int,int,int);
+void Print_info(vector<vector<rooms>>,int, status&);
+void setup_doors_items(vector<vector<rooms>>& ,int);
+void move_and_loseHP(status& ,status ,int, int, int);
+bool check(status,string,int);
+void use_item(status&,int);
+void pick_item(vector<vector<rooms>>&,status&);
+void display(vector<vector<rooms>>,int, int,int);
+
+
+
+#endif
+#ifndef _DATAS_H
+#define _DATAS_H
+
+#include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+//int S = 5;
+
+struct position
+{
+    int x,y;
+};
+struct status
+{
+    position pos;
+    int h, d, v;
+
+};
+struct doors
+{
+    int f, b, l, r;
+};
+
+struct rooms
+{
+    doors door;
+    string type = "N" ;
+    string item_name ="NULL";
+    int   pass = 0;
+};
+
+void add_door(rooms&,int);
+string random_item(int);
+void move(status& , status&);
+void define_items();
+void define_player_monster(int);
+status MakeStatus(int, int, int,int,int);
+void Print_info(vector<vector<rooms>>,int, status&);
+void setup_doors_items(vector<vector<rooms>>& ,int);
+void move_and_loseHP(status& ,status ,int, int, int);
+bool check(status,string,int);
+void use_item(status&,int);
+void pick_item(vector<vector<rooms>>&,status&);
+void display(vector<vector<rooms>>,int, int,int);
 
 
 
